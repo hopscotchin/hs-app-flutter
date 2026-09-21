51fa6ddecc13c12f5f10c85798556a8bc7bc30a7
prudhvi.kumar <prudhvi.kumar@hopscotch.in>
2026-09-21 12:29:50 +0530

    removed my kids

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -56,7 +56,7 @@ class AppRouter {
       ...OrdersRoute.getRoutes(_rootNavigatorKey),
       WishlistRoute.getRoute(_rootNavigatorKey),
       AddressRoute.getRoute(_rootNavigatorKey),
-      KidsRoute.getRoute(_rootNavigatorKey),
+      // KidsRoute.getRoute(_rootNavigatorKey),
       LegalRoute.getRoute(_rootNavigatorKey),
       PromoDetailsRoute.getRoute(_rootNavigatorKey),
       // ...CheckoutRoute.getRoutes(_rootNavigatorKey),
